705bdd735c2df0d35c31b9a07c9fe9e7dfbeae34
Davide Schiavone <davide@openhwgroup.org>
2022-02-04 23:48:52 +0100

    working on stdp modulated by dopamine - to debug

--- mnist_example.py
+++ mnist_example.py
@@ -67,10 +67,13 @@ def scanmatrix(mymatrix, myfile):
 figpath = './figures/'
 
 print_input_stream = False
-print_output_membrana = True
-print_weights = True
-print_traces = True
-print_reward_stream = True
+print_l1_membrana = False
+print_l1_weights = False
+print_l1_traces = False
+print_output_membrana = False
+print_weights = False
+print_traces = False
+print_reward_stream = False
 plt_example_mnist = False
 use_only_1_and_2 = False
 use_only_1_and_2_and_3 = False
@@ -260,20 +263,20 @@ else: ##keep the weights constant during learning of second layer or testing
                         method='linear')
 
 
-i_syn = []
-j_syn = []
+i_s010_syn = []
+j_s010_syn = []
 
 for n1 in range(N1_Neurons):
     #all N0 neurons connected to n1
     i_n = np.arange(0,N0_Neurons)
     j_n = np.ones(size(i_n))*n1
-    i_syn = np.concatenate((i_syn, i_n))
-    j_syn = np.concatenate((j_syn, j_n))
+    i_s010_syn = np.concatenate((i_s010_syn, i_n))
+    j_s010_syn = np.concatenate((j_s010_syn, j_n))
 
-i_syn = i_syn.astype(int)
-j_syn = j_syn.astype(int)
+i_s010_syn = i_s010_syn.astype(int)
+j_s010_syn = j_s010_syn.astype(int)
 
-S010.connect(i=i_syn, j=j_syn)
+S010.connect(i=i_s010_syn, j=j_s010_syn)
 
 if learning_1_phase:
     S010.wmax     = Thresholdl1*0.5
@@ -355,20 +358,20 @@ if learning_2_phase:
                         ''',
                         method='linear')
 
-    i_syn = []
-    j_syn = []
+    i_s120_syn = []
+    j_s120_syn = []
 
     for n2 in range(N2_Neurons):
         #all N1 neurons connected to n2
         i_n = np.arange(0,N1_Neurons)
         j_n = np.ones(size(i_n))*n2
-        i_syn = np.concatenate((i_syn, i_n))
-        j_syn = np.concatenate((j_syn, j_n))
+        i_s120_syn = np.concatenate((i_s120_syn, i_n))
+        j_s120_syn = np.concatenate((j_s120_syn, j_n))
 
-    i_syn = i_syn.astype(int)
-    j_syn = j_syn.astype(int)
+    i_s120_syn = i_s120_syn.astype(int)
+    j_s120_syn = j_s120_syn.astype(int)
 
-    S120.connect(i=i_syn, j=j_syn)
+    S120.connect(i=i_s120_syn, j=j_s120_syn)
 
     S120.wmax     = Thresholdl1*0.5
     S120.wmin     = 0
@@ -377,34 +380,39 @@ if learning_2_phase:
 
     NReward = SpikeGeneratorGroup(Reward_Neurons, [0], [0]*ms)
 
-    ##Reward has 10 Neurons, S120 has N1_Neurons*N2_Neurons connections
+    ##Reward has 2*N2_Neurons, S120 has N1_Neurons*N2_Neurons connections
     SRS120 = Synapses(NReward, S120,
                         '''
                          ''',
                         on_pre='''
-                        reward_post = int(i<N2_Neurons)
-                        punish_post = int(i>=N2_Neurons)
+                        reward_post = int(i<N2_Neurons)*1.0
+                        punish_post = int(i>=N2_Neurons)*1.0
                         ''',
                         method='linear')
 
 
-    i_syn = []
-    j_syn = []
+    i_srs120_syn = []
+    j_srs120_syn = []
 
-    for nr in range(Reward_Neurons):
-        #nr 0 connected to synapsys 0...N1_Neurons-1 which are the ones connected N1 to neuron 0 of N2
-        #nr 1 connected to synapsys N1...2*N1_Neurons-1 which are the ones connected N1 to neuron 1 of N2
+    for nr in range(N2_Neurons):
+        #nr 0  connected to synapsys 0...N1_Neurons-1 which are the ones connecting N1 to neuron 0 of N2 (reward)
+        #nr 10 connected to synapsys 0...N1_Neurons-1 which are the ones connecting N1 to neuron 10 of N2 (punish)
+        #nr 1 connected to synapsys N1...2*N1_Neurons-1 which are the ones connecting N1 to neuron 1 of N2
         # like a flat matrix, etc
-        offset = nr % N2_Neurons
-        j_n = np.arange(0,N1_Neurons)+ (offset*N1_Neurons)
+        j_n = np.arange(0,N1_Neurons) + (nr*N1_Neurons)
         i_n = np.ones(size(j_n))*nr
-        i_syn = np.concatenate((i_syn, i_n))
-        j_syn = np.concatenate((j_syn, j_n))
+        i_srs120_syn = np.concatenate((i_srs120_syn, i_n))
+        j_srs120_syn = np.concatenate((j_srs120_syn, j_n))
+        i_n = np.ones(size(j_n))*(nr+N2_Neurons)
+        i_srs120_syn = np.concatenate((i_srs120_syn, i_n))
+        j_srs120_syn = np.concatenate((j_srs120_syn, j_n))
 
-    i_syn = i_syn.astype(int)
-    j_syn = j_syn.astype(int)
 
-    SRS120.connect(i=i_syn, j=j_syn)
+    i_srs120_syn = i_srs120_syn.astype(int)
+    j_srs120_syn = j_srs120_syn.astype(int)
+
+
+    SRS120.connect(i=i_srs120_syn, j=j_srs120_syn)
 
     net.add(N2)
     net.add(NReward)
@@ -432,7 +440,7 @@ net.run(0*ms)
 weight_matrix_flat  = np.reshape(weight_matrix,(N0_Neurons*N1_Neurons))
 S010.w              = weight_matrix_flat
 weight_matrix2_flat = np.reshape(weight_matrix2,(N1_Neurons*N2_Neurons))
-S120.w              = weight_matrix2_flat/10/3*2
+S120.w              = weight_matrix2_flat
 
 
 n0_s_list = []
@@ -443,8 +451,6 @@ nr_t_list = []
 ts_time = 0
 i_count = 0
 
-stat_freq  = np.zeros((10, my_set_X_flat.shape[1]))
-stat_power = np.zeros((10, my_set_X_flat.shape[1]))
 
 for x_flat in my_set_X_flat:
 
@@ -461,7 +467,7 @@ for x_flat in my_set_X_flat:
 
     #reward
     nr_s    = []
-    nr_s.append(train_y[i_count] if train_y[i_count] <= 1 else 1)
+    nr_s.append(train_y[i_count])
 
     nr_t    = []
     nr_t.append(ts_time) ###reward
@@ -503,23 +509,26 @@ N2mon    = SpikeMonitor(N2)
 net.add(N2mon)
 
 
-if(print_output_membrana):
+if(print_l1_membrana):
     N1state  = StateMonitor(N1, ['v'], record=np.arange(N1_Neurons), dt=monitor_step*ms)
-    N2state  = StateMonitor(N2, ['v'], record=np.arange(N2_Neurons), dt=monitor_step*ms)
     net.add(N1state)
+
+if(print_output_membrana):
+    N2state  = StateMonitor(N2, ['v'], record=np.arange(N2_Neurons), dt=monitor_step*ms)
     net.add(N2state)
 
-if(print_traces or print_weights):
+if(print_l1_traces or print_l1_weights):
     if learning_1_phase:
         S010state   = StateMonitor(S010, ['w','apre', 'apost'], record=np.arange(N0_Neurons*N1_Neurons), dt=monitor_step*ms)
     else:
         S010state   = StateMonitor(S010, ['w'], record=np.arange(N0_Neurons*N1_Neurons), dt=monitor_step*ms)
+    net.add(S010state)
+
+if(print_traces or print_weights):
     if learning_2_phase:
-        S120state   = StateMonitor(S120, ['w','apre', 'apost', 'reward'], record=np.arange(N0_Neurons*N1_Neurons), dt=monitor_step*ms)
+        S120state   = StateMonitor(S120, ['w','apre', 'apost', 'reward', 'punish'], record=np.arange(N0_Neurons*N1_Neurons), dt=monitor_step*ms)
     else:
         S120state   = StateMonitor(S120, ['w'], record=np.arange(N1_Neurons*N2_Neurons), dt=monitor_step*ms)
-
-    net.add(S010state)
     net.add(S120state)
 
 net.run(set_size*single_example_time*ms)
@@ -576,7 +585,7 @@ if(print_reward_stream):
 
 step=int(plot_step_time*10);
 
-if(print_output_membrana):
+if(print_l1_membrana):
     plt.figure(1)
 
     for n1 in range(N1_Neurons):
@@ -611,11 +620,16 @@ if(print_output_membrana):
     plt.close(1)
 
 if(print_output_membrana):
-    plt.figure(1)
 
+    fig_counter = 1
     for n2 in range(N2_Neurons):
 
-        ax1 = plt.subplot2grid((N2_Neurons,1), (n2,0))
+        if (n2 % 4 == 0):
+            plt.figure(1)
+            fig_counter = fig_counter + 1
+
+
+        ax1 = plt.subplot2grid((4,1), (n2 % 4,0))
         ax1.set_title("Output Neuron Membrana neuron " + str(n2))
 
         time_plot = []
@@ -633,23 +647,36 @@ if(print_output_membrana):
 
         plt.plot(time_plot, state_plot, label='N2::'+str(n2))
 
+        if (n2 % 4 == 3):
+
+            plt.xlabel('Time (ms)')
+            plt.ylabel('V')
+            plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
+            plt.ylim((-0.22, 0.12))
+            plt.grid(True)
+            if not testing_phase:
+                plt.savefig(figpath + '9_output_membrana_reward_' + str(fig_counter-1) + '.png')
+            else:
+                plt.savefig(figpath + '9_test_output_membrana_reward_' + str(fig_counter-1) + '.png')
+            plt.close(1)
+
+    if ( (n2-1) % 4 < 3):
         plt.xlabel('Time (ms)')
         plt.ylabel('V')
         plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
         plt.ylim((-0.22, 0.12))
         plt.grid(True)
-    if not testing_phase:
-        plt.savefig(figpath + '9_output_membrana_reward.png')
-    else:
-        plt.savefig(figpath + '9_test_output_membrana_reward.png')
-    plt.close(1)
-
+        if not testing_phase:
+            plt.savefig(figpath + '9_output_membrana_reward_' + str(fig_counter-1) + '.png')
+        else:
+            plt.savefig(figpath + '9_test_output_membrana_reward_' + str(fig_counter-1) + '.png')
+        plt.close(1)
 
 
 weights_to_plot = np.arange(675,700+1)
 
 
-if(print_weights):
+if(print_l1_weights):
     plt.figure(1)
 
     for n1 in range(N1_Neurons):
@@ -716,16 +743,21 @@ if learning_1_phase:
         printmatrix(weight_matrix[n1],sourceFile)
         sourceFile.close()
 
-weights2_to_plot = np.arange(0,N1_Neurons)
+weights2_to_plot = np.arange(0,N1_Neurons, step=10)
 
 ##np.set_printoptions(threshold=sys.maxsize)
 
 if(print_weights):
     plt.figure(1)
 
+    fig_counter = 1
     for n2 in range(N2_Neurons):
 
-        ax1 = plt.subplot2grid((N2_Neurons,1), (n2,0))
+        if (n2 % 4 == 0):
+            plt.figure(1)
+            fig_counter = fig_counter + 1
+
+        ax1 = plt.subplot2grid((4,1), (n2 % 4,0))
         ax1.set_title("N" + str(n2) + " Weights - layer 2")
 
         min_w = +1
@@ -752,21 +784,34 @@ if(print_weights):
             state_plot = S120state.w[weights+(n2*N1_Neurons)][sample_time_index]
 
             plt.plot(time_plot, state_plot,label='N2::'+str(n2))
-            plt.grid(True)
 
+
+        if (n2 % 4 == 3):
+            plt.xlabel('Time (ms)')
+            plt.ylabel('Weights')
+            plt.ylim((min_w*0.9,max_w*1.1))
+            plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
+            plt.grid(True)
+            if learning_2_phase:
+                plt.savefig(figpath + '10_weights_stdp_reward_' + str(fig_counter-1) + '.png')
+            else:
+                plt.savefig(figpath + '10_test_weights_reward_' + str(fig_counter-1) + '.png')
+            plt.close(1)
+
+    if ( (n2-1) % 4 < 3):
         plt.xlabel('Time (ms)')
         plt.ylabel('Weights')
         plt.ylim((min_w*0.9,max_w*1.1))
         plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
+        plt.grid(True)
+        if learning_2_phase:
+            plt.savefig(figpath + '10_weights_stdp_reward_' + str(fig_counter-1) + '.png')
+        else:
+            plt.savefig(figpath + '10_test_weights_reward_' + str(fig_counter-1) + '.png')
+        plt.close(1)
 
-    if learning_2_phase:
-        plt.savefig(figpath + '10_weights_stdp_reward.png')
-    else:
-        plt.savefig(figpath + '10_test_weights_reward.png')
 
-    plt.close(1)
-
-if(print_traces and learning_1_phase):
+if(print_l1_traces and learning_1_phase):
     plt.figure(1)
 
     for n1 in range(N1_Neurons):
@@ -805,9 +850,14 @@ if(print_traces and learning_1_phase):
 if(print_traces and learning_2_phase):
     plt.figure(1)
 
+    fig_counter = 1
     for n2 in range(N2_Neurons):
 
-        ax1 = plt.subplot2grid((N2_Neurons,1), (n2,0))
+        if (n2 % 4 == 0):
+            plt.figure(1)
+            fig_counter = fig_counter + 1
+
+        ax1 = plt.subplot2grid((4,1), (n2 % 4,0))
         ax1.set_title("N" + str(n2) + " Traces")
 
         for weights in weights2_to_plot:
@@ -815,6 +865,7 @@ if(print_traces and learning_2_phase):
             state_plot = []
             state2_plot = []
             state3_plot = []
+            state4_plot = []
 
             sample_time_condition = (S120state.t/ms < end_plot*single_example_time) & (S120state.t/ms >= plot_start_time*single_example_time)
             sample_time_index     = np.where(sample_time_condition)[0]
@@ -827,24 +878,36 @@ if(print_traces and learning_2_phase):
             state_plot = np.concatenate((state_plot, S120state.apre[weights+(n2*N1_Neurons)][sample_time_index]))
             state2_plot = np.concatenate((state2_plot, S120state.apost[weights+(n2*N1_Neurons)][sample_time_index]))
             state3_plot = np.concatenate((state3_plot, S120state.reward[weights+(n2*N1_Neurons)][sample_time_index]))
-
+            state4_plot = np.concatenate((state4_plot, S120state.punish[weights+(n2*N1_Neurons)][sample_time_index]))
 
             plt.plot(time_plot, state_plot, label='N2::'+str(n2))
             plt.plot(time_plot, state2_plot, label='N2::'+str(n2))
             plt.plot(time_plot, state3_plot*0.0004, label='N2::'+str(n2), color='r')
-            plt.grid(True)
+            plt.plot(time_plot, -state4_plot*0.0004, label='N2::'+str(n2), color='b')
+
+
+        if (n2 % 4 == 3):
             plt.xlabel('Time (ms)')
             plt.ylabel('Traces')
             plt.ylim((-0.0012,+0.0008))
             plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
-    plt.savefig(figpath + '11_traces_stdp_reward.png')
-    plt.close(1)
+            plt.grid(True)
+            plt.savefig(figpath + '11_traces_stdp_reward' + str(fig_counter-1) + '.png')
+            plt.close(1)
+
+    if ( (n2-1) % 4 < 3):
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Traces')
+        plt.ylim((-0.0012,+0.0008))
+        plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
+        plt.grid(True)
+        plt.savefig(figpath + '11_traces_stdp_reward' + str(fig_counter-1) + '.png')
+        plt.close(1)
+
 
 
 plt.figure(1)
-
-plt.title("Output Neuron Stream")
-
+plt.title("L1 Neuron Stream")
 for i_count in np.arange(plot_start_time,end_plot):
 
     color = classes_color[train_y[i_count]]
@@ -853,20 +916,41 @@ for i_count in np.arange(plot_start_time,end_plot):
         N1mon_times_nk_plot   = N1mon.spike_trains()[k][sample_time_condition]
         N1mon_nspikes_nk_plot = np.ones(size(N1mon_times_nk_plot))*k
         plt.plot(N1mon_times_nk_plot/ms, N1mon_nspikes_nk_plot, '*'+color)
-
-
 plt.ylim((0,N1_Neurons))
 plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
-
 plt.xlabel('Time (ms)')
 plt.ylabel('Neuron index');
 plt.grid(True)
 if not testing_phase:
-    plt.savefig(figpath + '12_output_neurons.png')
+    plt.savefig(figpath + '12_l1_neurons.png')
 else:
-    plt.savefig(figpath + '12_test_output_neurons.png')
+    plt.savefig(figpath + '12_test_l1_neurons.png')
 
 plt.close(1)
+
+plt.figure(1)
+plt.title("Output Neuron Stream")
+for i_count in np.arange(plot_start_time,end_plot):
+
+    color = classes_color[train_y[i_count]]
+    for k in range(N2_Neurons):
+        sample_time_condition = (N2mon.spike_trains()[k]/ms < (i_count+1)*single_example_time) & (N2mon.spike_trains()[k]/ms >= (i_count)*single_example_time)
+        N2mon_times_nk_plot   = N2mon.spike_trains()[k][sample_time_condition]
+        N2mon_nspikes_nk_plot = np.ones(size(N2mon_times_nk_plot))*k
+        plt.plot(N2mon_times_nk_plot/ms, N2mon_nspikes_nk_plot, '*'+color)
+plt.ylim((0,N2_Neurons))
+plt.xlim((plot_start_time*single_example_time, end_plot*single_example_time))
+plt.xlabel('Time (ms)')
+plt.ylabel('Neuron index');
+plt.grid(True)
+if not testing_phase:
+    plt.savefig(figpath + '13_output_neurons.png')
+else:
+    plt.savefig(figpath + '13_test_output_neurons.png')
+
+plt.close(1)
+
+
 
 exit();
 
